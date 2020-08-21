9274dc44bef29e20d7f3768ada1b5ffe47202dec
Panchev <neno.panchev@gmail.com>
2020-08-21 12:08:34 +0300

    update

--- Java Advanced/Advanced/Generics/src/Lab/JarOfT/Jar.java
+++ Java Advanced/Advanced/Generics/src/Lab/JarOfT/Jar.java
@@ -1,4 +1,20 @@
 package Lab.JarOfT;
 
-public class Jar {
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Jar<T> {
+    private Deque<T> data;
+
+    public Jar() {
+        this.data = new ArrayDeque<>();
+    }
+
+    public void add(T element) {
+        this.data.push(element);
+    }
+
+    public T remove() {
+        return this.data.poll();
+    }
 }
--- Java Advanced/Advanced/Generics/src/Lab/JarOfT/Main.java
+++ Java Advanced/Advanced/Generics/src/Lab/JarOfT/Main.java
@@ -2,6 +2,16 @@ package Lab.JarOfT;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
+        Jar<Integer> jar = new Jar<>();
+        jar.add(1);
+        jar.add(2);
+        jar.add(3);
+        System.out.println(jar.remove());
+
+        Jar<String> strJar = new Jar<>();
+        strJar.add("one");
+        strJar.add("two");
+        strJar.add("three");
+        System.out.println(strJar.remove());
     }
 }
