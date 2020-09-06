3e8df999d66bb7171a9b28465b9899681510ed1a
Panchev <neno.panchev@gmail.com>
2020-09-06 14:27:18 +0300

    Java Advanced Exam practicing

--- Java Advanced/Advanced/JavaAdvancedExam/src/BookWorm.java
+++ Java Advanced/Advanced/JavaAdvancedExam/src/BookWorm.java
@@ -1,12 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BookWorm {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String initialString = scan.nextLine();
+        StringBuilder initialString = new StringBuilder(scan.nextLine());
         int sideOfMatrix = Integer.parseInt(scan.nextLine());
         String[][] matrix = readMatrix(scan, sideOfMatrix);
+        List<Integer> currentPosition = new ArrayList<>();
+        for (int rows = 0; rows < matrix.length; rows++) {
+            for (int cols = 0; cols < matrix[0].length; cols++) {
+                if (matrix[rows][cols].equals("P")) {
+                    currentPosition.add(rows);
+                    currentPosition.add(cols);
+                }
+            }
+        }
+        String direction = scan.nextLine();
 
+        while (!"end".equals(direction)) {
+            if (isValidIndex(matrix, getDirection(currentPosition, direction))) {
+                matrix[currentPosition.get(0)][currentPosition.get(1)] = "-";
+                currentPosition = getDirection(currentPosition, direction);
+                String nextSymbol = matrix[currentPosition.get(0)][currentPosition.get(1)];
+                if (!nextSymbol.equals("-")) {
+                    initialString.append(nextSymbol);
+                }
+                matrix[currentPosition.get(0)][currentPosition.get(1)] = "P";
+            } else {
+                initialString.deleteCharAt(initialString.length() - 1);
+            }
+            direction = scan.nextLine();
+        }
+        System.out.println(initialString.toString());
+        printMatrix(matrix);
     }
 
     private static String[][] readMatrix(Scanner scan, int side) {
@@ -15,5 +43,44 @@ public class BookWorm {
             matrix[rows] = scan.nextLine().split("");
         }
         return matrix;
+    }
+
+    private static boolean isValidIndex(String[][] matrix, List<Integer> list) {
+        int nextRow = list.get(0);
+        int nextCol = list.get(1);
+        return nextRow >= 0 && nextRow < matrix.length && nextCol >= 0 && nextCol < matrix.length;
+    }
+
+    private static List<Integer> getDirection(List<Integer> initialList, String direction) {
+        List<Integer> list = new ArrayList<>();
+        list.add(initialList.get(0));
+        list.add(initialList.get(1));
+        switch (direction) {
+            case "up":
+                list.set(0, list.get(0) - 1);
+                break;
+
+            case "down":
+                list.set(0, list.get(0) + 1);
+                break;
+
+            case "left":
+                list.set(1, list.get(1) - 1);
+                break;
+
+            default:
+                list.set(1, list.get(1) + 1);
+                break;
+        }
+        return list;
+    }
+
+    private static void printMatrix(String[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.printf("%s", matrix[row][col]);
+            }
+            System.out.println();
+        }
     }
 }
