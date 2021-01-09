8307886ec90b36775208291e2b86ec2f84383018
Panchev <neno.panchev@gmail.com>
2021-01-09 16:33:44 +0200

    Basic CRUD

---
+++ Java DB/Basic CRUD/geography 21-23.sql
@@ -0,0 +1,21 @@
+#21
+SELECT `p`.`peak_name` 
+	FROM `peaks` AS `p`
+	ORDER BY `p`.`peak_name`;
+	
+#22
+SELECT `c`.`country_name`, `c`.`population` 
+	FROM `countries` AS `c`
+	WHERE `c`.`continent_code` LIKE 'EU'
+	ORDER BY 
+		`c`.`population` DESC,
+		`c`.`country_name`
+	LIMIT 30;
+	
+#23
+SELECT `country_name`, `country_code`,
+IF(`currency_code` = 'EUR', 'Euro', 'Not Euro') AS `currency`
+	FROM `countries`
+    ORDER BY `country_name`;
+	
+	
