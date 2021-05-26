ca89f05381c94aabeae54f92e3d593787ddd0e87
Panchev <neno.panchev@gmail.com>
2021-05-26 12:37:33 +0300

    trying controller ex handling vs global ex handling

--- Java Web/Spring Advanced/07. Error Handling/src/main/java/spring/errorhandling/global/HelloController2.java
+++ Java Web/Spring Advanced/07. Error Handling/src/main/java/spring/errorhandling/global/HelloController2.java
@@ -1,15 +1,34 @@
 package spring.errorhandling.global;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class HelloController2 {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HelloController2.class);
 
   @GetMapping("/hello2-ex-custom")
   public ModelAndView customException() {
     throw new HelloException("HELLO fro Controller 2!");
   }
 
+  @GetMapping("/global-vs-controller")
+  public ModelAndView gvc() {
+    throw new HelloException("Controller wins!");
+  }
+
+  @ExceptionHandler({HelloException.class})
+  public ModelAndView helloExceptionHandle(HelloException he) {
+    ModelAndView modelAndView = new ModelAndView("handler");
+    modelAndView.addObject("message", he.getMessage());
+
+    LOGGER.error("Exception caught", he);
+    return modelAndView;
+  }
 }
